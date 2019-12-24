c08b0cd17058a05301aab14b252c64494c90fdb5
gjyhuang <grace.jy.huang@gmail.com>
2019-12-23 19:46:24 -0500

    added two number sum

--- arrays/twoNumberSum.js
+++ arrays/twoNumberSum.js
@@ -1,9 +1,18 @@
+/*Two Number Sum
+
+Write a function that takes in a non-empty array of distinct integers and a target sum.
+If any two numbers in the input array add up to the target sum, the function should return them in an array.
+If no two numbers add up to the target sum, the function should return an empty array.
+Only return at most one pair of numbers which add up to the target sum.
+
+*/
+
 function twoNumberSum(array, targetSum) {
   const nums = {};
 	for (let i = 0; i < array.length; i++) {
 		let curr = array[i];
 		if (nums[curr]) {
-			return [nums[curr], curr]
+			return [nums[curr], curr];
 		}
 		else {
 			nums[targetSum-curr] = curr;
