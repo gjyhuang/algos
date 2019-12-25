51d7864fbf4d2f812b447607e875b7b770983467
gjyhuang <grace.jy.huang@gmail.com>
2019-12-24 21:49:17 -0500

    added depth-first search

---
+++ graphs/depthFirstSearch.js
@@ -0,0 +1,31 @@
+/* Depth-first Search
+
+Given a Node class that has a name and array of optional children Nodes, implement
+the depthFirstSearch method on the Node class, which takes in an empty array,
+traverses the tree using the DFS approach (from left to right), stores all of
+the Nodes' names in the input array, and returns it.
+
+O(v+e) time | O(v) space, where v is the number of vertices of the input graph
+and e is the number of edges of the input graph
+
+*/
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+		array.push(this.name);
+		for (const child of this.children) {
+			child.depthFirstSearch(array);
+		}
+		return array;
+  }
+}
