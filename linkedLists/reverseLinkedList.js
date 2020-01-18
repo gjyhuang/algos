9c3c5143337f5e14eece59fc3b475c1d4848189c
gjyhuang <grace.jy.huang@gmail.com>
2020-01-17 19:11:20 -0500

    added reverse linked list

--- linkedLists/reverseLinkedList.js
+++ linkedLists/reverseLinkedList.js
@@ -7,12 +7,24 @@ the list and return its new head. Every node has a "value" and "next" property.
 
 O(n) time | O(1) space
 
-// make a new node that refers to the head, and a var reversed set to null
-// while node is not null, keep setting nodes onto the new chain
-	// set var curr to hold node.next
-	// set node.next to equal reversed
-	//
 
+0 -> 1 -> 2 -> 3
+
+current 0  - .val = 0, .next = 1      nextNode = null
+reversed 0 - .val = 0, .next = null   nextNode = 0
+  save the 1, will be the next node in the while loop
+
+current 3  - .val = 3, .next = null   nextNode = 2
+reversed 3 - .val = 3, .next = 2      nextNode = 3
+
+
+// make a new node that refers to the head
+// set a var nextNode set to null - will keep reassigning .next to this
+// while node is not null, keep reversing nodes
+	// set nowPrevNode to hold node.next
+	// set node.next to equal nextNode and nextNode to equal node
+  // set node to nowPrevNode
+// return nextNode
 */
 
 function reverseLinkedList(head) {
